edcc85931ec932995023d81b178c3b25e4b00215
Davidoffili <palagashvilid@gmail.com>
2020-05-24 17:16:18 -0700

    added route to pass a JSON object thru the server

--- server.js
+++ server.js
@@ -7,3 +7,17 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('./public'));
+
+app.get('/hello', (request, response) => {
+    response.status(200).send('Behold, Tha Get Route!');
+});
+
+app.get('/data', (request, response) => {
+    let h4X = {
+        target: 'my wallet',
+        dispatch: Date.now(),
+        happend2u: [true, 'my identity has been stolen twice but the theif was caught on an atm cam when they got greedy! 15 years in federal prison!'],
+        justiceServed: true,
+    };
+    response.status(200).json(h4X);
+});
