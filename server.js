2607d9dc74ef20e5b6ec8031e4ac5c0bf89e78a8
Davidoffili <palagashvilid@gmail.com>
2020-05-24 18:35:19 -0700

    eliminating excess code - troubleshooting heroku delpoyment

--- server.js
+++ server.js
@@ -8,20 +8,4 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static('./public'));
 
-app.get('/hello', (request, response) => {
-    response.status(200).send('Hello World!');
-});
-
-app.get('/data', (request, response) => {
-    let h4X = {
-        target: 'my wallet',
-        dispatch: Date.now(),
-        happend2u: [true, 'my identity has been stolen twice but the theif was caught on an atm cam when they got greedy! 15 years in federal prison!'],
-        justiceServed: true,
-    };
-    response.status(200).json(h4X);
-});
-
-app.use('*', (request, response) => response.send('Yeah... you kinda drove off a cliff - NON EXISTENT ROUTE!'));
-
 app.listen(PORT,() => console.log(`Listening to Andre${PORT}, pretty exclusive stuff!`));
