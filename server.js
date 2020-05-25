e727679499a3b6abd9c50630c2b1ca4eb7fe9953
Davidoffili <palagashvilid@gmail.com>
2020-05-24 17:04:59 -0700

    started server work with boiler plate, updated README

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+//boilerplate
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('./public'));
